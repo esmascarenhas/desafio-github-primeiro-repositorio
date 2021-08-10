3acd3e241e9dd80d5e3682fe44af1152f7bfacdd
esmascarenhas <esmascarenhas12@gmail.com>
2021-08-09 21:37:39 -0300

    Novo pacote Calculo do IMC

--- situacaoAluno/src/calculoIMC/Imc.java
+++ situacaoAluno/src/calculoIMC/Imc.java
@@ -16,9 +16,22 @@ public class Imc {
         pessoa.altura = dados.nextDouble();
         Double calculoImc = pessoa.imc();
 
-
-
         System.out.println(calculoImc);
+        if (calculoImc >= 40) {
+            System.out.println("Misericordia! Obesidade Morbida ");
+        } else if (calculoImc >= 35 && calculoImc < 40) {
+            System.out.println("Vixe! Obesidade Severa ");
+        }else if (calculoImc >= 30 && calculoImc < 35) {
+            System.out.println("Cuidado! Obesidade ");
+        }else if (calculoImc >= 25 && calculoImc < 30) {
+            System.out.println("Você está com Sobrepeso ");
+        }else if (calculoImc >= 18.5 && calculoImc < 25) {
+            System.out.println("Parabéns! Você está no seu peso ideal. ");
+        } else if (calculoImc >= 17 && calculoImc < 18.5) {
+            System.out.println("Abaixo do Peso ");
+        } else {
+            System.out.println("Muito abaixo do peso");
+        }
     }
 
 }
